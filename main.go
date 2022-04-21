e030a40bb52a2243947a78bbaaca5f4f92aa1127
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-04-21 10:51:23 -0300

    Renderizando templates

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", Handle)
-	fmt.Println(http.ListenAndServe(":8080", nil))
+type Post struct {
+	Id    int
+	Title string
+	Body  string
 }
 
+func main() {
+	http.HandleFunc("/", Handle)                   // registrando uma função para ser executada ao ter uma requisição no path /
+	fmt.Println(http.ListenAndServe(":8080", nil)) // subindo e servindo uma aplicação
+}
+
+// func Handle é adicionada como função para ser executada ao ter uma requisição no path /
 func Handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	t := template.Must(template.ParseFiles("templates/index.html")) // registrando um template para ser servido
+	if err := t.ExecuteTemplate(w, "index.html", nil); err != nil { // verificando se houve erro na execução do template
+		http.Error(w, err.Error(), http.StatusInternalServerError) // se houver erro, manda um status code 500
+	}
 }
