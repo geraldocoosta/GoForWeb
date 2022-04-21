30462d6a37404de48d1a2c50a1ddb0b4c0e4660c
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-04-21 10:38:22 -0300

    Fazendo uma página servida com go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", Handle)
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
