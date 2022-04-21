fec9bfc6374c6286c6eed2f0523799460f7ec807
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-04-21 12:15:13 -0300

    Inserindo informações no banco

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Post struct {
@@ -12,9 +15,28 @@ type Post struct {
 	Body  string
 }
 
+var db, error = sql.Open("mysql", "root:root@/go-course?charset=utf8")
+
 func main() {
+
+	db.Ping() // verificando se houve erro na conexão com o banco de dados
+
+	insertValue()
+
 	http.HandleFunc("/", Handle)                   // registrando uma função para ser executada ao ter uma requisição no path /
 	fmt.Println(http.ListenAndServe(":8080", nil)) // subindo e servindo uma aplicação
+}
+
+func insertValue() {
+	stmt, error := db.Prepare("INSERT INTO posts(title, body) VALUES(?, ?)") // preparando a query para ser executada
+	if error != nil {
+		panic(error)
+	}
+
+	_, error = stmt.Exec("My second post", "This is my second post") // executando a query
+	if error != nil {
+		panic(error)
+	}
 }
 
 // func Handle é adicionada como função para ser executada ao ter uma requisição no path /
