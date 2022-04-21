ca49827c4218949588d312c96acccf0811102525
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-04-21 11:48:10 -0300

    Informações dinamicas

--- main.go
+++ main.go
@@ -19,8 +19,18 @@ func main() {
 
 // func Handle é adicionada como função para ser executada ao ter uma requisição no path /
 func Handle(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("templates/index.html")) // registrando um template para ser servido
-	if err := t.ExecuteTemplate(w, "index.html", nil); err != nil { // verificando se houve erro na execução do template
+	post := Post{
+		Id:    1,
+		Title: "My first post",
+		Body:  "This is my first post",
+	}
+
+	if r.FormValue("title") != "" {
+		post.Title = r.FormValue("title")
+	}
+
+	t := template.Must(template.ParseFiles("templates/index.html"))  // registrando um template para ser servido
+	if err := t.ExecuteTemplate(w, "index.html", post); err != nil { // verificando se houve erro na execução do template, passando o post pro template
 		http.Error(w, err.Error(), http.StatusInternalServerError) // se houver erro, manda um status code 500
 	}
 }
